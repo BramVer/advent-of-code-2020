84d1b0a62d5770b45c59413e3670643e3a4ca6ff
Bram Vereertbrugghen <bram@adimian.com>
2020-12-02 18:51:34 +0100

    Day-two exercise two

--- nim-lang/day_two.nim
+++ nim-lang/day_two.nim
@@ -5,25 +5,49 @@ from sequtils import map
 const fname = "day_two_input.txt"
 const separator = ": "
 const policy_separator = " "
-const between_separator = "-"
+const positions_separator = "-"
 
 
-proc isBetween(between: string, value: int): bool =
-    let parsed = between.split(between_separator).map(parseInt)
-    parsed[0] <= value and value <= parsed[1]
+block firstAssFinement:
+    proc isBetween(between: string, value: int): bool =
+        let parsed = between.split(positions_separator).map(parseInt)
+        parsed[0] <= value and value <= parsed[1]
 
 
-proc isValidPassword(between: string, substr: string, pwd: string): bool =
-    isBetween(between, pwd.count(substr))
+    proc isValidPassword(between: string, substr: string, pwd: string): bool =
+        isBetween(between, pwd.count(substr))
 
 
-proc extractValidPasswords(): int =
-    for line in fname.lines:
-        let splitted = line.split(separator)
-        let policy = splitted[0].split(policy_separator)
+    proc extractValidPasswords(): int =
+        for line in fname.lines:
+            let splitted = line.split(separator)
+            let policy = splitted[0].split(policy_separator)
 
-        if isValidPassword(policy[0], policy[1], splitted[1]):
-            result += 1
+            if isValidPassword(policy[0], policy[1], splitted[1]):
+                result += 1
 
 
-echo extractValidPasswords()
+    echo extractValidPasswords()
+
+
+
+block secondAssFinement:
+    proc isCharAtPosInString(subchar: char, pos: int, value: string): bool =
+        pos - 1 >= low(value) and pos - 1 <= high(value) and value[pos - 1] == subchar
+
+    proc isValidPassword(positions: string, subchar: char, pwd: string): bool =
+        let parsed = positions.split(positions_separator).map(parseInt)
+        isCharAtPosInString(subchar, parsed[0], pwd) xor isCharAtPosInString(
+                subchar, parsed[1], pwd)
+
+
+    proc extractValidPasswords(): int =
+        for line in fname.lines:
+            let splitted = line.split(separator)
+            let policy = splitted[0].split(policy_separator)
+
+            if isValidPassword(policy[0], policy[1][0], splitted[1]):
+                result += 1
+
+
+    echo extractValidPasswords()
