a155254fe13e9d4fa085d2692b69b1f26bd5891a
Bram Vereertbrugghen <bramvereertbrugghen@live.be>
2020-12-06 18:47:36 +0100

    Daze hun klote kunnen trekken dikke zever

--- nim-lang/day_four.nim
+++ nim-lang/day_four.nim
@@ -1,10 +1,9 @@
-import nre
+import re
 import strutils
 
 const fname = "day_four_input.txt"
 const rules: seq[string] = @[
     "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"
-    #, "cid"
 ]
 let input = fname.readFile().split("\n\n")
 
@@ -24,119 +23,124 @@ let input = fname.readFile().split("\n\n")
 
 
 proc getInformation(passport: string, substr: string): string =
-    for pp in passport.split(" "):
+    for pp in passport.replace("\n", " ").split(" "):
         if pp.split(":")[0] == substr:
             return pp.split(":")[1]
 
 proc isByr(passport: string): bool =
     let byr = getInformation(passport, "byr")
-    return (
-        len(byr) == 4 and
-        parseInt(byr) >= 1920 and
-        parseInt(byr) <= 2002
+    var number = parseInt(byr)
+    var output = (
+        len(byr) == 4 and number >= 1920 and number <= 2002
     )
 
+    if not output:
+        echo "Passport failed for BYR test with number: ", number
+
+    return output
+
+
 proc isIyr(passport: string): bool =
-    let tyr = getInformation(passport, "tyr")
-    return (
-        len(tyr) == 4 and
-        parseInt(tyr) >= 2010 and
-        parseInt(tyr) <= 2020
+    let iyr = getInformation(passport, "iyr")
+    var number = parseInt(iyr)
+    var output = (
+        len(iyr) == 4 and number >= 2010 and number <= 2020
     )
+
+    if not output:
+        echo "Passport failed for IYR test with number: ", number
+
+    return output
 
 proc isEyr(passport: string): bool =
     let eyr = getInformation(passport, "eyr")
-    return (
-        len(eyr) == 4 and
-        parseInt(eyr) >= 2020 and
-        parseInt(eyr) <= 2030
+    var number = parseInt(eyr)
+    var output = (
+        len(eyr) == 4 and number >= 2020 and number <= 2030
     )
+
+    if not output:
+        echo "Passport failed for EYR test with number: ", number
+
+    return output
+
 
 proc isHgt(passport: string): bool =
     let hgt = getInformation(passport, "hgt")
-    return (
-        (hgt[^2..^1] == "cm" and
-        parseInt(hgt[0..^2]) >= 150 and
-        parseInt(hgt[0..^2]) <= 193) or
-
-        (hgt[^2..^1] == "in" and
-        parseInt(hgt[0..^2]) >= 59 and
-        parseInt(hgt[0..^2]) <= 76)
+    var number = parseInt(hgt[0..^3])
+    var output = (
+        (hgt[^2..^1] == "cm" and number >= 150 and number <= 193) or
+        (hgt[^2..^1] == "in" and number >= 59 and number <= 76)
     )
 
+    if not output:
+        echo "Passport failed for HGT test with number: ", number
+
+    return output
+
 proc isHcl(passport: string): bool =
-    var hcl = getInformation(passport, "hcl")
-    for dink in hcl:
-        if dink == "g"[0]:
-            return false
-        elif dink == "h"[0]:
-            return false
-        elif dink == "i"[0]:
-            return false
-        elif dink == "j"[0]:
-            return false
-        elif dink == "k"[0]:
-            return false
-        elif dink == "l"[0]:
-            return false
-        elif dink == "m"[0]:
-            return false
-        elif dink == "n"[0]:
-            return false
-        elif dink == "o"[0]:
-            return false
-        elif dink == "p"[0]:
-            return false
-        elif dink == "q"[0]:
-            return false
-        elif dink == "r"[0]:
-            return false
-        elif dink == "s"[0]:
-            return false
-        elif dink == "t"[0]:
-            return false
-        elif dink == "u"[0]:
-            return false
-        elif dink == "v"[0]:
-            return false
-        elif dink == "w"[0]:
-            return false
-        elif dink == "x"[0]:
-            return false
-        elif dink == "y"[0]:
-            return false
-        elif dink == "z"[0]:
-            return false
-        else:
-            if dink != "a"[0] and dink != "b"[0] and dink != "c"[0] and dink !=
-                    "d"[0] and dink != "e"[0] and dink != "f"[0]:
-                if dink != "0"[0] and dink != "1"[0] and dink != "2"[0] and
-                        dink != "3"[0] and dink != "4"[0] and dink != "5"[0] and
-                        dink != "6"[0] and dink != "7"[0] and dink != "8"[0] and
-                        dink != "9"[0]:
-                    return false
+    var hcl = getInformation(passport, "hcl").strip()
+    if hcl[0] != '#':
+        return false
+    var matchk = passport.find(re"#(?:[0-9a-fA-F]{3}){1,2}")
+    var output = matchk != -1
 
-    return true
+    if output == false:
+        echo "Passport failed for HCL test with haircolor: ", hcl
+    else:
+        echo "Passport worked for HCL test with haircolor: ", hcl
 
+    return output
 
 proc isEcl(passport: string): bool =
     let ecl = getInformation(passport, "ecl")
-    return ecl in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+    var output = ecl in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+
+    if not output:
+        echo "Passport failed for ECL test with eyecolor: ", ecl
+
+    return output
 
 proc isPid(passport: string): bool =
     let pid = getInformation(passport, "pid")
-    return (pid.len == 9 and parseInt(pid) >= 0)
+    var number = parseInt(pid)
+    var output = (len(pid) == 9 and number >= 0)
+
+    if not output:
+        echo "Passport failed for PID test with : ", number
+
+    return output
 
 
-const rerules: array[7, proc] = [isByr, isIyr, isEyr, isHgt, isHcl, isEcl, isPid]
+# var rerules: array[7, proc] = [isByr, isIyr, isEyr, isHgt, isHcl, isEcl, isPid]
 proc isCompliant(passport: string): bool =
     for rule in rules:
         if rule notin passport:
             return false
 
-    for rerule in rerules:
-        if not rerule(passport):
-            return false
+    # for rerule in rerules:
+    #     if not rerule(passport):
+    #         return false
+
+    # echo "\nStudying passport:\n", passport
+    try:
+        # if not isByr(passport):
+        #     echo "Not isByr"
+        # if not isIyr(passport):
+        #     echo "Not isIyr"
+        # if not isEyr(passport):
+        #     echo "Not isEyr"
+        # if not isHgt(passport):
+        #     echo "Not isHgt"
+        if isHcl(passport) == false:
+            echo "Not isHcl"
+        # if not isEcl(passport):
+        #     echo "Not isEcl"
+        # if not isPid(passport):
+        #     echo "Not isPid"
+    except Exception as e:
+        echo "We have exc: ", e.msg
+        return false
 
     return true
 
--- nim-lang/dink.nim
+++ nim-lang/dink.nim
@@ -1,3 +1,16 @@
 import re
 
 let dink = re"hallo"
+
+
+echo """hhallo jij!""".find(dink)
+
+
+proc isHcl(passport: string): bool =
+    var output = passport.find(re"^#(?:[0-9a-fA-F]{3}){1,2}$")
+    return output != -1
+
+echo isHcl("#ceb3a1")
+echo isHcl("#FFF")
+echo isHcl("#abcdef")
+echo isHcl("##ceb3a1")
